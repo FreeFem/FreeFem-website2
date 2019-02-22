1a9f3b22ae073864ab3a6ace62168fd7f9e03b8b
Simon Garnotel <simon.garnotel@gmail.com>
2019-02-22 11:03:10 +0100

    add github release version

---
+++ js/githubRelease.js
@@ -0,0 +1,12 @@
+fetch('https://api.github.com/repos/FreeFem/FreeFem-sources/releases/latest')
+	.then(response => {
+		if (response.ok) {
+			return response.json()
+		} else {
+			return Promise.reject('something went wrong!')
+		}
+	})
+	.then(data => {
+		document.getElementById("release").innerHTML = "RELEASE " + data.tag_name"
+	})
+	.catch(error => document.getElementById("release").innerHTML = "Grab the latest version");
