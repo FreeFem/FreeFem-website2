eaa364b7da008370313115304b91e6c8da28ed67
karlaprzbr <karla.perezbrugues@gmail.com>
2019-02-27 15:09:21 +0100

    improve open console js

--- js/openConsole.js
+++ js/openConsole.js
@@ -2,12 +2,15 @@ let open = false;
 
 function openConsole() {
   const exConsole = document.getElementById('exampleConsole')
+  const temp = document.getElementById('main')
 
   if (open) {
+    temp.style.gridTemplateRows = "auto 39px"
     exConsole.style.height = "39px"
     exConsole.style.overflow = "hidden"
     open = false
   } else {
+    temp.style.gridTemplateRows = "70% 30%"
     exConsole.style.height = "100%"
     exConsole.style.overflow = "auto"
     open = true
