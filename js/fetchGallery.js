e4772ecedb30541cab49c7ce947b1960531b6b74
Simon Garnotel <simon.garnotel@gmail.com>
2019-03-11 12:10:15 +0100

    activate gallery

--- js/fetchGallery.js
+++ js/fetchGallery.js
@@ -1,6 +1,6 @@
 window.addEventListener('popstate', function(event) {
 	if (history.state) {
-		fetchModule(event, history.state.url, history.state.title, true)
+		fetchGallery(event, history.state.url, history.state.title, true)
 		event.preventDefault()
 		event.stopPropagation()
 	}
@@ -13,8 +13,13 @@ function fetchGallery(e, url, title, disableHistory) {
 			return response.text()
 		})
 		.then(function(html) {
+			// Get content (not surrounding header, footer, nav, ...)
+			var parser = new DOMParser()
+			var content = parser.parseFromString(html, "text/html")
+			var galleryImg = content.getElementById('galleryImg')
+
 			// Replace in the page
-			document.querySelector('#galleryImg').innerHTML = html
+			document.querySelector('#galleryImg').innerHTML = galleryImg.innerHTML
 
 			// Reset scroll
 			document.body.scrollTop = 0
@@ -25,9 +30,26 @@ function fetchGallery(e, url, title, disableHistory) {
 			document.title = "FREEFEM++ - " + title
 
 			// Relaunch MathJax
-			MathJax.Hub.Queue(["Typeset",MathJax.Hub])
+			try{
+				MathJax.Hub.Queue(["Typeset",MathJax.Hub])
+			} catch (error) {
+				console.log('MathJax disabled');
+				console.log(error)
+			}
 		})
 		.catch(function(err) {
 			console.log('Failed to fetch page: ', err)
 		})
+}
+
+function activateGallery(elmt) {
+	const elmtToActivate = elmt.parentNode.parentNode
+
+	const galleryTitle = document.getElementById('galleryTitle')
+	const list = galleryTitle.getElementsByTagName('li')
+
+	for (let i = 0; i < list.length; i++) {
+		list[i].classList.remove('active')
+	}
+	elmtToActivate.classList.add('active')
 }
